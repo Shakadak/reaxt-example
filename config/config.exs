7273fb49623bbbbb00d2a748b30f629e70601784
shakadak <nathanael.pineau@gmail.com>
2018-06-10 20:14:38 +0200

    now, only have to do a version that can use react 16, and another framework

--- config/config.exs
+++ config/config.exs
@@ -1,5 +1,8 @@
 use Mix.Config
 
-config :reaxt, otp_app: :hello
-config :reaxt, hot: Mix.env == :dev
-config :reaxt, pool_size: if(Mix.env == :dev, do: 1, else: 10)
+config :reaxt, [
+  otp_app: :hello,
+  reaxt_js_root: "reaxt-react-15",
+  hot: Mix.env == :dev,
+  pool_size: if(Mix.env == :dev, do: 1, else: 10)
+]
--- web/webpack.config.js
+++ web/webpack.config.js
@@ -1,9 +1,9 @@
 const path = require('path')
 
-const reaxt-js-root = 'reaxt-react-15'
+const reaxtJsRoot = 'reaxt-react-15'
 
 const client_config = {
-  entry: `${reaxt-js-root}/client_entry_addition`,
+  entry: `${reaxtJsRoot}/client_entry_addition`,
   output: {
     path: path.join(__dirname, '../priv/static'), //typical output on the default directory served by Plug.Static
     filename: 'client.[hash].js', //dynamic name for long term caching, or code splitting, use WebPack.file_of(:main) to get it
@@ -25,7 +25,7 @@ const client_config = {
 
 const server_config = Object.assign(Object.assign({},client_config),{
   target: "node",
-  entry: `${reaxt-js-root}/react_server`,
+  entry: `${reaxtJsRoot}/react_server`,
   externals: {},
   output: {
     path: path.join(__dirname, '../priv/react_servers'), //typical output on the default directory served by Plug.Static
